2f0e70ce45a9a93e1030f933ed6f0c6eeab2f45e
RishabhRawatAndroid <rishicentury1996@gmail.com>
2018-06-24 18:37:59 +0530

    Adding the Score board

--- app/src/main/java/com/myapp/rishabhrawat/quizapp/FinalScore.java
+++ app/src/main/java/com/myapp/rishabhrawat/quizapp/FinalScore.java
@@ -5,8 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.nio.MappedByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FinalScore extends AppCompatActivity {
 
+    DatabaseReference reference;
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,15 @@ public class FinalScore extends AppCompatActivity {
 
         textView=findViewById(R.id.textView);
 
+        reference= FirebaseDatabase.getInstance().getReference().child("questionsBank1").child("UserScores");
         Intent intent=getIntent();
         textView.setText("Total score is "+     intent.getIntExtra("correct_ans",0));
+        String random=reference.push().getKey(); //at place of random id we can put the gmail id
+        Map<String,Object> map=new HashMap<>();
+        map.put("score",intent.getIntExtra("correct_ans",0));
+        reference.child(random).setValue(map);
+
+
+
     }
 }
